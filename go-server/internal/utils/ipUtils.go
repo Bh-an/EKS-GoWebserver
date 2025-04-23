acbbef4eb7baae58c5c298f5846a26779b67b98f
Bh-an <bhan.16070@gmail.com>
2025-04-23 12:21:47 +0530

    Testing tagging with version

--- go-server/internal/utils/ipUtils.go
+++ go-server/internal/utils/ipUtils.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-// Function for getting location from X-Location attribute of HTTP header
+// Function for getting location from 'X-Timezone' attribute of HTTP header
 func GetLocation(r *http.Request) *time.Location {
 
 	timezoneName := r.Header.Get("X-Timezone")
@@ -29,7 +29,7 @@ func GetLocation(r *http.Request) *time.Location {
 	return location
 }
 
-// Function for getting client IP from X-Forwaded-For attribute of HTTP header
+// Function for getting client IP from the 'X-Forwaded-For' attribute of HTTP header
 func GetIP(r *http.Request) string {
 	forwarded := r.Header.Get("X-Forwarded-For")
 	if forwarded != "" {
